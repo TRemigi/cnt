b0813cc51b555de688ff2cbda4d876ab0e7257e1
TRemigi <tayremigi@gmail.com>
2025-03-12 22:01:17 -0600

    cnt

---
+++ cnt.go
@@ -0,0 +1,53 @@
+// Package cnt writes a countdown to stdout, then sends a desktop notification when complete.
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/gen2brain/beeep"
+	"log"
+	"os"
+	"time"
+)
+
+var dArg int
+var uArg time.Duration
+var nArg bool
+var qArg bool
+
+func main() {
+	flag.IntVar(&dArg,"duration", 5, "countdown duration")
+	flag.DurationVar(&uArg, "unit", time.Second, "unit")
+	flag.BoolVar(&nArg, "notify", false, "notify after cnt completes")
+	flag.BoolVar(&qArg, "quiet", false, "don't output to console")
+	flag.Parse()
+
+	dur := time.Duration(dArg) * uArg
+	cntdn(dur, uArg, nArg)
+}
+
+func cntdn(d time.Duration, u time.Duration, n bool) {
+	if d > 0 {
+		quietPrint(d)
+		time.Sleep(u)
+		cntdn(d-u, u, n)
+	} else {
+		fmt.Fprintln(os.Stdout, "CNT complete")
+		if n {
+			notify()
+		}
+	}
+}
+
+func quietPrint(d time.Duration) {
+	if !qArg {
+		fmt.Printf("%s\n", d)
+	}
+}
+
+func notify() {
+	err := beeep.Notify("CNT", "Complete", "./img/taco.png")
+	if err != nil {
+		log.Fatalf("cnt failed: %v", err)
+	}
+}
